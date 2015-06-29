1c377ad54de2b2e40f5b551f2472b9b5b2104de1
stephanmg <stephan@syntaktischer-zucker.de>
2015-06-29 12:49:51 +0200

    Initial commit.

---
+++ src/main/java/de/zucker/syntaktischer/utils/FileUtils.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/// package's name
+package de.zucker.syntaktischer.utils;
+
+/// imports
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+/**
+ *
+ * @author stephanmg <stephan@syntaktischer-zucker.de>
+ */
+public class FileUtils {
+	@SuppressWarnings("NestedAssignment")
+	private static void copyFile(File source, File dest) throws IOException {
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			is = new FileInputStream(source);
+			os = new FileOutputStream(dest);
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = is.read(buffer)) > 0) {
+				os.write(buffer, 0, length);
+			}
+		} finally {
+			is.close();
+			os.close();
+		}
+	}
+
+}
